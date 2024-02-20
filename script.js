265609cc702b285347e7aab925aae6690c995751
Ryan Flynn <122565977+ryanflynn-dev@users.noreply.github.com>
2024-02-20 19:39:01 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
-<<<<<<< HEAD
-=======
-fetch("./json/courses.json")
-  .then((response) => response.json())
-  .then((data) => {  })
-  .catch((error) => console.error("Error loading the course data:", error));
-
->>>>>>> d0b931da1eacbae9d869aceb21cdeca933a846cc
 function searchCourses(searchTerm) {
   fetch("./json/courses.json")
     .then((response) => response.json())
